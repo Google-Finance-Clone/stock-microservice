58ca46f1af12f1fa9003b3eb7ebd929cb6a9eaf0
unknown <imostafa1000@gmail.com>
2023-05-05 16:35:38 +0200

    project template

---
+++ src/main/java/com/stocksmicroservice/StocksMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.stocksmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StocksMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StocksMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stocksmicroservice/StocksMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stocksmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StocksMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
